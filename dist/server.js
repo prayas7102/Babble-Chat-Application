fa75572392dbf5a182c76388cb1a29fd87082478
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-11-01 10:08:31 +0530

    frontend & gitignore

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
 dotenv.config({ path: '/ChatApp/config.env' });
 const app = express();
 const port = process.env.PORT;
