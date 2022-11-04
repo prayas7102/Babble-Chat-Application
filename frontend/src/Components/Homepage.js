90a90a9595a053064441b04dd6bb00267136d2f0
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-11-04 11:22:38 +0530

    homepage ui changes

--- frontend/src/Components/Homepage.js
+++ frontend/src/Components/Homepage.js
@@ -1,18 +1,40 @@
 import React from 'react'
-import { Button, Container, Box, Text } from '@chakra-ui/react'
+import { Tab, Tabs, TabList, Container, Box, Text } from '@chakra-ui/react'
 
 const Homepage = () => {
   return (
     <div>
-      <Container>
+      <Container maxW="xl" centerContent>
         <Box
-          d='flex' justifyContent="center" p={3} bg={"white"}
-          w="100%" m="40px 0 15px 0"
-          borderRadius="lg" borderWidth="1px"
+          d="flex"
+          justifyContent="center"
+          p={3}
+          bg="white"
+          w="100%"
+          m="40px 0 15px 0"
+          borderRadius="lg"
+          borderWidth="1px"
         >
-          <Text> Baat Karega?</Text>
+          <Text fontSize="4xl" fontFamily="Work sans">
+            Talk-A-Tive
+          </Text>
         </Box>
-        <Button colorScheme='blue'>Button</Button>
+        <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+          <Tabs isFitted variant="soft-rounded">
+            <TabList mb="1em">
+              <Tab>Login</Tab>
+              <Tab>Sign Up</Tab>
+            </TabList>
+            {/* <TabPanels>
+              <TabPanel>
+                <Login />
+              </TabPanel>
+              <TabPanel>
+                <Signup />
+              </TabPanel>
+            </TabPanels> */}
+          </Tabs>
+        </Box>
       </Container>
     </div>
   )
